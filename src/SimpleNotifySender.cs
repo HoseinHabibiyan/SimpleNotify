fbcb1bfc73bf633758122b9d6d68aa53e15a4111
Hosein Habibiyan <hn.habibiyan@gmail.com>
2025-05-23 11:03:07 +0330

    Implement simple notify sender to discover & handle notifications

---
+++ src/SimpleNotifySender.cs
@@ -0,0 +1,17 @@
+﻿using SimpleNotify.Contracts;
+
+namespace SimpleNotify;
+
+public class SimpleNotifySender : ISimpleNotifySender
+{
+    public async ValueTask Publish<TNotification>(TNotification notification) where TNotification : INotification<TNotification>
+    {
+        List<Type> types = ServiceRegistrar.Assemblies.SelectMany(x => x.GetTypes())
+            .Where(x => x.IsClass && x.IsAssignableTo(typeof(INotificationHandler<TNotification>))).ToList();
+
+        foreach (var handler in types.Select(type => Activator.CreateInstance(type) as INotificationHandler<TNotification>))
+        {
+            await handler!.Handle(notification);
+        }
+    }
+}
