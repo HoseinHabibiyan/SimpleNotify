5e024816f1c8f770967694fc2cd4de1218fb416f
h.habibiyan <h.habibiyan@asax.ir>
2026-02-07 18:33:09 +0330

    refactor: scan and register handlers as scope life time

--- src/ServiceRegistrar.cs
+++ src/ServiceRegistrar.cs
@@ -6,14 +6,45 @@ namespace SimpleNotify;
 
 public static class ServiceRegistrar
 {
-    public static Assembly[] Assemblies = [];
-    
-    public static void AddSimpleNotify(this IServiceCollection services,params Assembly[] assembliesToScan)
+    public static void AddSimpleNotify(this IServiceCollection services, Assembly[] assembliesToScan)
+        => services.RegisterHandlers(assembliesToScan);
+
+
+    public static void AddSimpleNotify<TAssembly>(this IServiceCollection services) where TAssembly : class
+    {
+        var assembly = typeof(TAssembly).Assembly;
+        services.RegisterHandlers([assembly]);
+    }
+
+
+    private static void RegisterHandlers(this IServiceCollection services, Assembly[] assembliesToScan)
     {
         if (assembliesToScan is null || !assembliesToScan.Any())
-            throw new ArgumentException($"[SimpleNotify::{nameof(AddSimpleNotify)}] Argument '{nameof(assembliesToScan)}' cannot be null or empty.");
-        
+            throw new ArgumentException(
+                $"[SimpleNotify::{nameof(AddSimpleNotify)}] Argument '{nameof(assembliesToScan)}' cannot be null or empty.");
+
         services.AddScoped<ISimpleNotifySender, SimpleNotifySender>();
-        Assemblies = assembliesToScan.Distinct().ToArray();
+        assembliesToScan = assembliesToScan.Distinct().ToArray();
+
+        var handlerInterface = typeof(INotificationHandler<>);
+
+        var handlers = assembliesToScan
+            .SelectMany(a => a.GetTypes())
+            .Where(t =>
+                t is { IsClass: true, IsAbstract: false } &&
+                t.GetInterfaces().Any(i =>
+                    i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == handlerInterface));
+
+        foreach (var handler in handlers)
+        {
+            var interfaces = handler.GetInterfaces()
+                .Where(i =>
+                    i.IsGenericType &&
+                    i.GetGenericTypeDefinition() == handlerInterface);
+
+            foreach (var @interface in interfaces)
+                services.AddScoped(@interface, handler);
+        }
     }
 }
