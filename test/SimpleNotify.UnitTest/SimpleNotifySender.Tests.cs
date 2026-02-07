1f9dfdc5558c13c26f220a741d5f9282c9f12db5
h.habibiyan <h.habibiyan@asax.ir>
2026-02-07 18:37:08 +0330

    test: fix unit tests

--- test/SimpleNotify.UnitTest/SimpleNotifySender.Tests.cs
+++ test/SimpleNotify.UnitTest/SimpleNotifySender.Tests.cs
@@ -7,31 +7,23 @@ namespace SimpleNotify.UnitTest;
 
 public class SimpleNotifySenderTests
 {
-    record AddOrderNotification(string OrderNumber,int Quantity) : INotification<AddOrderNotification>;
-    record AddOrderNotificationHandler(TextWriter Writer) : INotificationHandler<AddOrderNotification>
-    {
-        public ValueTask Handle(AddOrderNotification notification)
-        {
-            Writer.WriteLine($"{notification} :: {Guid.NewGuid()}");
-            return ValueTask.CompletedTask;
-        }
-    }
-    
     [Fact]
     public async Task When_AddOrderNotification_Is_Published_Should_Invoke_Handler_As_Expected()
     {
         #region Arrange
-        
+
         var builder = new StringBuilder();
         var writer = new StringWriter(builder);
 
-        SimpleNotifySender simpleNotifySender = new();
-        
-        AddOrderNotification orderNotification = new (Guid.NewGuid().ToString(), new Random().Next());
-        
         ServiceCollection serviceCollection = new();
-        serviceCollection.AddSimpleNotify(typeof(SimpleNotifySender).Assembly);
-        
+        ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+        SimpleNotifySender simpleNotifySender = new(serviceProvider);
+
+        AddOrderNotification orderNotification = new(Guid.NewGuid().ToString(), new Random().Next());
+
+        serviceCollection.AddSimpleNotify<SimpleNotifySender>();
+
         INotificationHandler<AddOrderNotification> handler = new AddOrderNotificationHandler(writer);
         await handler.Handle(orderNotification);
 
@@ -40,39 +32,41 @@ public class SimpleNotifySenderTests
         #region Act
 
         await simpleNotifySender.Publish(orderNotification);
-        
+
         #endregion
 
         #region Assert
 
         string result = builder.ToString();
-        
+
         result.ShouldContain(orderNotification.ToString());
 
         #endregion
     }
-    
+
     [Fact]
     public async Task When_AddOrderNotification_Is_Published_And_We_Have_MultipleHandlers_Should_Invoke_AllHandlers_As_Expected()
     {
         #region Arrange
-        
+
         var builder = new StringBuilder();
         var writer = new StringWriter(builder);
 
-        SimpleNotifySender simpleNotifySender = new();
-        
         ServiceCollection serviceCollection = new();
-        serviceCollection.AddSimpleNotify(typeof(SimpleNotifySender).Assembly);
-        
-        AddOrderNotification orderNotification = new (Guid.NewGuid().ToString(), new Random().Next());
+        ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+        SimpleNotifySender simpleNotifySender = new(serviceProvider);
+
+        serviceCollection.AddSimpleNotify<SimpleNotifySender>();
+
+        AddOrderNotification orderNotification = new(Guid.NewGuid().ToString(), new Random().Next());
 
         INotificationHandler<AddOrderNotification> handler1 = new AddOrderNotificationHandler(writer);
         await handler1.Handle(orderNotification);
-        
+
         INotificationHandler<AddOrderNotification> handler2 = new AddOrderNotificationHandler(writer);
         await handler2.Handle(orderNotification);
-        
+
         INotificationHandler<AddOrderNotification> handler3 = new AddOrderNotificationHandler(writer);
         await handler3.Handle(orderNotification);
 
@@ -81,7 +75,7 @@ public class SimpleNotifySenderTests
         #region Act
 
         await simpleNotifySender.Publish(orderNotification);
-        
+
         #endregion
 
         #region Assert
@@ -91,18 +85,29 @@ public class SimpleNotifySenderTests
         var expected = result.Split("\n")
             .Where(x => !string.IsNullOrWhiteSpace(x))
             .Select(x => x.Substring(0, orderNotification.ToString().Length)).ToList();
-        
+
         expected.Count.ShouldBe(3);
-        expected.ShouldAllBe(x=>x.Substring(0, orderNotification.ToString().Length) == orderNotification.ToString());
-        expected.GroupBy(x=>x).Count().ShouldBe(1);
+        expected.ShouldAllBe(x => x.Substring(0, orderNotification.ToString().Length) == orderNotification.ToString());
+        expected.GroupBy(x => x).Count().ShouldBe(1);
 
         #endregion
     }
-    
+
     [Fact]
     public void When_AddSimpleNotify_NotPassed_AssembliesToScan_Argument_Then_Should_Throw_Argument_Exception()
     {
         ServiceCollection services = new();
-        Should.Throw<ArgumentException>(() => services.AddSimpleNotify());
+        Should.Throw<ArgumentException>(() => services.AddSimpleNotify([]));
+    }
+
+    private record AddOrderNotification(string OrderNumber, int Quantity) : INotification<AddOrderNotification>;
+
+    private record AddOrderNotificationHandler(TextWriter Writer) : INotificationHandler<AddOrderNotification>
+    {
+        public ValueTask Handle(AddOrderNotification notification)
+        {
+            Writer.WriteLine($"{notification} :: {Guid.NewGuid()}");
+            return ValueTask.CompletedTask;
+        }
     }
 }
